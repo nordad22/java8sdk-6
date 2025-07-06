42f84000e6fb49fe411c4b51f85930bf6a54fac6
nordad22 <79371316+nordad22@users.noreply.github.com>
2025-07-06 14:32:40 +0800

    Add files via upload

--- index.mjs
+++ index.mjs
@@ -1,274 +1,182 @@
-/* eslint-disable complexity */
-import { promises as fs } from 'node:fs';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { DefaultArtifactClient } from '@actions/artifact';
 import * as core from '@actions/core';
-import { exec } from '@actions/exec';
 import { context, getOctokit } from '@actions/github';
-import * as glob from '@actions/glob';
-import * as io from '@actions/io';
-import { markdownTable } from 'markdown-table';
-import { getArtifactsForBranchAndWorkflow } from './utils/getArtifactsForBranchAndWorkflow.mjs';
-import { SizeLimitFormatter } from './utils/SizeLimitFormatter.mjs';
-
-const SIZE_LIMIT_HEADING = '## size-limit report 📦 ';
-const ARTIFACT_NAME = 'size-limit-action';
-const RESULTS_FILE = 'size-limit-results.json';
-
-function getResultsFilePath() {
-  const __dirname = path.dirname(fileURLToPath(import.meta.url));
-  return path.resolve(__dirname, RESULTS_FILE);
-}
-
-const { getInput, setFailed } = core;
-
-async function fetchPreviousComment(octokit, repo, pr) {
-  const { data: commentList } = await octokit.rest.issues.listComments({
-    ...repo,
-    issue_number: pr.number,
-  });
-
-  const sizeLimitComment = commentList.find(comment => comment.body.startsWith(SIZE_LIMIT_HEADING));
-  return !sizeLimitComment ? null : sizeLimitComment;
-}
-
-async function execSizeLimit() {
-  let output = '';
-
-  const status = await exec('yarn run --silent size-limit --json', [], {
-    windowsVerbatimArguments: false,
-    ignoreReturnCode: true,
-    cwd: process.cwd(),
-    listeners: {
-      stdout: data => {
-        output += data.toString();
-      },
-    },
-  });
-
-  return { status, output };
-}
 
 async function run() {
-  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const { getInput } = core;
 
-  try {
-    const { payload, repo } = context;
-    const pr = payload.pull_request;
+  const { repo, owner } = context.repo;
 
-    const comparisonBranch = getInput('comparison_branch');
-    const githubToken = getInput('github_token');
-    const threshold = getInput('threshold');
+  const githubToken = getInput('github_token');
+  const clearDevelop = inputToBoolean(getInput('clear_develop', { type: 'boolean' }));
+  const clearBranches = inputToBoolean(getInput('clear_branches', { type: 'boolean', default: true }));
+  const clearPending = inputToBoolean(getInput('clear_pending_prs', { type: 'boolean' }));
+  const workflowName = getInput('workflow_name');
 
-    if (comparisonBranch && !pr) {
-      throw new Error('No PR found. Only pull_request workflows are supported.');
-    }
+  const octokit = getOctokit(githubToken);
 
-    const octokit = getOctokit(githubToken);
-    const limit = new SizeLimitFormatter();
-    const resultsFilePath = getResultsFilePath();
-
-    // If we have no comparison branch, we just run size limit & store the result as artifact
-    if (!comparisonBranch) {
-      return runSizeLimitOnComparisonBranch();
-    }
-
-    // Else, we run size limit for the current branch, AND fetch it for the comparison branch
-    let base;
-    let current;
-    let baseIsNotLatest = false;
-    let baseWorkflowRun;
-
-    try {
-      const workflowName = `${process.env.GITHUB_WORKFLOW || ''}`;
-      core.startGroup(`getArtifactsForBranchAndWorkflow - workflow:"${workflowName}",  branch:"${comparisonBranch}"`);
-      const artifacts = await getArtifactsForBranchAndWorkflow(octokit, {
-        ...repo,
-        artifactName: ARTIFACT_NAME,
-        branch: comparisonBranch,
-        workflowName,
-      });
-      core.endGroup();
-
-      if (!artifacts) {
-        throw new Error('No artifacts found');
-      }
-
-      baseWorkflowRun = artifacts.workflowRun;
-
-      await downloadOtherWorkflowArtifact(octokit, {
-        ...repo,
-        artifactName: ARTIFACT_NAME,
-        artifactId: artifacts.artifact.id,
-        downloadPath: __dirname,
-      });
-
-      base = JSON.parse(await fs.readFile(resultsFilePath, { encoding: 'utf8' }));
-
-      if (!artifacts.isLatest) {
-        baseIsNotLatest = true;
-        core.info('Base artifact is not the latest one. This may lead to incorrect results.');
-      }
-    } catch (error) {
-      core.startGroup('Warning, unable to find base results');
-      core.error(error);
-      core.endGroup();
-    }
-
-    const { status, output } = await execSizeLimit();
-    try {
-      current = limit.parseResults(output);
-    } catch (error) {
-      core.error('Error parsing size-limit output. The output should be a json.');
-      throw error;
-    }
-
-    const thresholdNumber = Number(threshold);
-
-    const sizeLimitComment = await fetchPreviousComment(octokit, repo, pr);
-
-    if (sizeLimitComment) {
-      core.debug('Found existing size limit comment, updating it instead of creating a new one...');
-    }
-
-    const shouldComment =
-      isNaN(thresholdNumber) || limit.hasSizeChanges(base, current, thresholdNumber) || sizeLimitComment;
-
-    if (shouldComment) {
-      const bodyParts = [SIZE_LIMIT_HEADING];
-
-      if (baseIsNotLatest) {
-        bodyParts.push(
-          '⚠️ **Warning:** Base artifact is not the latest one, because the latest workflow run is not done yet. This may lead to incorrect results. Try to re-run all tests to get up to date results.',
-        );
-      }
-      try {
-        bodyParts.push(markdownTable(limit.formatResults(base, current)));
-      } catch (error) {
-        core.error('Error generating markdown table');
-        core.error(error);
-      }
-
-      if (baseWorkflowRun) {
-        bodyParts.push('');
-        bodyParts.push(`[View base workflow run](${baseWorkflowRun.html_url})`);
-      }
-
-      const body = bodyParts.join('\r\n');
-
-      try {
-        if (!sizeLimitComment) {
-          await octokit.rest.issues.createComment({
-            ...repo,
-            issue_number: pr.number,
-            body,
-          });
-        } else {
-          await octokit.rest.issues.updateComment({
-            ...repo,
-            comment_id: sizeLimitComment.id,
-            body,
-          });
-        }
-      } catch (error) {
-        core.error(
-          "Error updating comment. This can happen for PR's originating from a fork without write permissions.",
-        );
-      }
-    } else {
-      core.debug('Skipping comment because there are no changes.');
-    }
-
-    if (status > 0) {
-      try {
-        const results = limit.parseResults(output);
-        const failedResults = results
-          .filter(result => result.passed || false)
-          .map(result => ({
-            name: result.name,
-            size: +result.size,
-            sizeLimit: +result.sizeLimit,
-          }));
-
-        if (failedResults.length > 0) {
-          // eslint-disable-next-line no-console
-          console.log('Exceeded size-limits:', failedResults);
-        }
-      } catch {
-        // noop
-      }
-
-      setFailed('Size limit has been exceeded.');
-    }
-  } catch (error) {
-    core.error(error);
-    setFailed(error.message);
-  }
+  await clearGithubCaches(octokit, {
+    repo,
+    owner,
+    clearDevelop,
+    clearPending,
+    clearBranches,
+    workflowName,
+  });
 }
 
-async function runSizeLimitOnComparisonBranch() {
-  const __dirname = path.dirname(fileURLToPath(import.meta.url));
-  const resultsFilePath = getResultsFilePath();
+/**
+ * Clear caches.
+ *
+ * @param {ReturnType<import("@actions/github").getOctokit> } octokit
+ * @param {{repo: string, owner: string, clearDevelop: boolean, clearPending: boolean, clearBranches: boolean, workflowName: string}} options
+ */
+async function clearGithubCaches(octokit, { repo, owner, clearDevelop, clearPending, clearBranches, workflowName }) {
+  let deletedCaches = 0;
+  let remainingCaches = 0;
 
-  const limit = new SizeLimitFormatter();
-  const artifactClient = new DefaultArtifactClient();
+  let deletedSize = 0;
+  let remainingSize = 0;
 
-  const { output: baseOutput } = await execSizeLimit();
+  /** @type {Map<number, ReturnType<typeof octokit.rest.pulls.get>>} */
+  const cachedPrs = new Map();
+  /** @type {Map<string, ReturnType<typeof octokit.rest.actions.listWorkflowRunsForRepo>>} */
+  const cachedWorkflows = new Map();
 
-  try {
-    const base = limit.parseResults(baseOutput);
-    await fs.writeFile(resultsFilePath, JSON.stringify(base), 'utf8');
-  } catch (error) {
-    core.error('Error parsing size-limit output. The output should be a json.');
-    throw error;
+  /**
+   * Clear caches.
+   *
+   * @param {{ref: string}} options
+   */
+  const shouldClearCache = async ({ ref }) => {
+    // Do not clear develop caches if clearDevelop is false.
+    if (!clearDevelop && ref === 'refs/heads/develop') {
+      core.info('> Keeping cache because it is on develop.');
+      return false;
+    }
+
+    // There are two fundamental paths here:
+    // If the cache belongs to a PR, we need to check if the PR has any pending workflows.
+    // Else, we assume the cache belongs to a branch, where we do not check for pending workflows
+    const pullNumber = /^refs\/pull\/(\d+)\/merge$/.exec(ref)?.[1];
+    const isPr = !!pullNumber;
+
+    // Case 1: This is a PR, and we do not want to clear pending PRs
+    // In this case, we need to fetch all PRs and workflow runs to check them
+    if (isPr && !clearPending) {
+      const pr =
+        cachedPrs.get(pullNumber) ||
+        (await octokit.rest.pulls.get({
+          owner,
+          repo,
+          pull_number: pullNumber,
+        }));
+      cachedPrs.set(pullNumber, pr);
+
+      const prBranch = pr.data.head.ref;
+
+      // Check if PR has any pending workflows
+      const workflowRuns =
+        cachedWorkflows.get(prBranch) ||
+        (await octokit.rest.actions.listWorkflowRunsForRepo({
+          repo,
+          owner,
+          branch: prBranch,
+        }));
+      cachedWorkflows.set(prBranch, workflowRuns);
+
+      // We only care about the relevant workflow
+      const relevantWorkflowRuns = workflowRuns.data.workflow_runs.filter(workflow => workflow.name === workflowName);
+
+      const latestWorkflowRun = relevantWorkflowRuns[0];
+
+      core.info(`> Latest relevant workflow run: ${latestWorkflowRun.html_url}`);
+
+      // No relevant workflow? Clear caches!
+      if (!latestWorkflowRun) {
+        core.info('> Clearing cache because no relevant workflow was found.');
+        return true;
+      }
+
+      // If the latest run was not successful, keep caches
+      // as either the run may be in progress,
+      // or failed - in which case we may want to re-run the workflow
+      if (latestWorkflowRun.conclusion !== 'success') {
+        core.info(`> Keeping cache because latest workflow is ${latestWorkflowRun.conclusion}.`);
+        return false;
+      }
+
+      core.info(`> Clearing cache because latest workflow run is ${latestWorkflowRun.conclusion}.`);
+      return true;
+    }
+
+    // Case 2: This is a PR, but we do want to clear pending PRs
+    // In this case, this cache should always be cleared
+    if (isPr) {
+      core.info('> Clearing cache of every PR workflow run.');
+      return true;
+    }
+
+    // Case 3: This is not a PR, and we want to clean branches
+    if (clearBranches) {
+      core.info('> Clearing cache because it is not a PR.');
+      return true;
+    }
+
+    // Case 4: This is not a PR, and we do not want to clean branches
+    core.info('> Keeping cache for non-PR workflow run.');
+    return false;
+  };
+
+  for await (const response of octokit.paginate.iterator(octokit.rest.actions.getActionsCacheList, {
+    owner,
+    repo,
+  })) {
+    if (!response.data.length) {
+      break;
+    }
+
+    for (const { id, ref, size_in_bytes } of response.data) {
+      core.info(`Checking cache ${id} for ${ref}...`);
+
+      const shouldDelete = await shouldClearCache({ ref });
+
+      if (shouldDelete) {
+        core.info(`> Clearing cache ${id}...`);
+
+        deletedCaches++;
+        deletedSize += size_in_bytes;
+
+        await octokit.rest.actions.deleteActionsCacheById({
+          owner,
+          repo,
+          cache_id: id,
+        });
+      } else {
+        remainingCaches++;
+        remainingSize += size_in_bytes;
+      }
+    }
   }
 
-  const globber = await glob.create(resultsFilePath, {
-    followSymbolicLinks: false,
+  const format = new Intl.NumberFormat('en-US', {
+    style: 'decimal',
   });
-  const files = await globber.glob();
 
-  await artifactClient.uploadArtifact(ARTIFACT_NAME, files, __dirname);
+  core.info('Summary:');
+  core.info(`Deleted ${deletedCaches} caches, freeing up ~${format.format(deletedSize / 1000 / 1000)} mb.`);
+  core.info(`Remaining ${remainingCaches} caches, using ~${format.format(remainingSize / 1000 / 1000)} mb.`);
 }
 
 run();
 
-/**
- * Use GitHub API to fetch artifact download url, then
- * download and extract artifact to `downloadPath`
- */
-async function downloadOtherWorkflowArtifact(octokit, { owner, repo, artifactId, artifactName, downloadPath }) {
-  const artifact = await octokit.rest.actions.downloadArtifact({
-    owner,
-    repo,
-    artifact_id: artifactId,
-    archive_format: 'zip',
-  });
-
-  // Make sure output path exists
-  try {
-    await io.mkdirP(downloadPath);
-  } catch {
-    // ignore errors
+function inputToBoolean(input) {
+  if (typeof input === 'boolean') {
+    return input;
   }
 
-  const downloadFile = path.resolve(downloadPath, `${artifactName}.zip`);
+  if (typeof input === 'string') {
+    return input === 'true';
+  }
 
-  await exec('wget', [
-    '-nv',
-    '--retry-connrefused',
-    '--waitretry=1',
-    '--read-timeout=20',
-    '--timeout=15',
-    '-t',
-    '0',
-    '-O',
-    downloadFile,
-    artifact.url,
-  ]);
-
-  await exec('unzip', ['-q', '-d', downloadPath, downloadFile], {
-    silent: true,
-  });
+  return false;
 }
